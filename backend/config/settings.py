acb4d86582aa8a704d7af00a4c49d99af4fed46f
prenciarlinda-webwise <prenciarlinda@gmail.com>
2026-05-01 09:12:07 +0200

    Set STATIC_ROOT so collectstatic works in production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,6 +99,7 @@ USE_TZ = True
 LANGUAGES = [("sq", "Shqip"), ("en", "English")]
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
